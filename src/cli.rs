911ba4f4ad903fcea05b4c8dfc8fcdbb0151d157
robert arles <robert.r.arles@disney.com>
2020-06-27 15:23:05 -0700

    updated patch version

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,7 @@ use clap::{Arg, App};
 
 pub fn cli_arg_handler <'main> () -> clap::ArgMatches<'main> {
     return App::new("notesman")
-        .version("0.5.2")
+        .version("0.5.3")
         .arg(
             Arg::with_name("todo_file")
                 .help("The source todo file to be processed.")
